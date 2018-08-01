68d42625c8e13a111223ffa4b13ccb0504dca816
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 11:07:31 +0100

    feat: Create FilerControls component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import FilterControls from './FilterControls'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <FilterControls />
       </div>
     );
   }
---
+++ src/FilterControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FilterControls = () => (
+  <div>
+      Controls go in here...
+  </div>
+);
+
+export default FilterControls;

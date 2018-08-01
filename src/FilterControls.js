dbd66fb1ebbc1f5152707302dd2b93968542d884
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 11:37:42 +0100

    feat: Create LocationSelector component

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import LocationSelector from './LocationSelector';
 
 const FilterControls = () => (
   <div>
-      Controls go in here...
+    <LocationSelector />
   </div>
 );
 
---
+++ src/LocationSelector.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LocationSelector = () => <div>Pick a location...</div>;
+
+export default LocationSelector;

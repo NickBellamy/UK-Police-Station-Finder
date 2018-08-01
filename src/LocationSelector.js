fea6b1ca9aec5db8ca1df01d63bfd17d0ed967cc
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 12:01:43 +0100

    feat: Add basic HTML to LocationSelector

--- src/LocationSelector.js
+++ src/LocationSelector.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const LocationSelector = () => <div>Pick a location...</div>;
+const LocationSelector = () => (
+  <div>
+    <h2>Location</h2>
+    <input type="text" placeholder="Enter an address..." />
+    <input type="button" value="Search" />
+    <p>OR</p>
+    <select>
+      <option>County name...</option>
+    </select>
+  </div>
+);
 
 export default LocationSelector;

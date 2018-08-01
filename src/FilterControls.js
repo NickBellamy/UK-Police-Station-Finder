3db6ba8d7059a4d6e42f1c260db23677f75bbd27
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 11:43:39 +0100

    feat: Create NeighbourhoodInfo component

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LocationSelector from './LocationSelector';
 import FilterByCrime from './FilterByCrime';
+import NeighbourhoodInfo from './NeighbourhoodInfo';
 
 const FilterControls = () => (
   <div>
     <LocationSelector />
     <FilterByCrime />
+    <NeighbourhoodInfo />
   </div>
 );
 
---
+++ src/NeighbourhoodInfo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NeighbourhoodInfo = () => <div>Neighbourhood Info...</div>;
+
+export default NeighbourhoodInfo;

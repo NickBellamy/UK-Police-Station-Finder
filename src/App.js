0413836c6d6b9b9bc6af4964f93b174ec87976d9
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 11:09:11 +0100

    feat: Create Map component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import FilterControls from './FilterControls'
+import FilterControls from './FilterControls';
+import Map from './Map';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <FilterControls />
+        <Map />
       </div>
     );
   }

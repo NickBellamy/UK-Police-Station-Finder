4e2ee5d40f2b427b0999a4bf9e0b6469d555bcf4
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-01 12:27:25 +0100

    feat: Add basic HTML to NeighbourhoodInfo

--- src/NeighbourhoodInfo.js
+++ src/NeighbourhoodInfo.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
-const NeighbourhoodInfo = () => <div>Neighbourhood Info...</div>;
+const NeighbourhoodInfo = () => (
+  <div>
+    <h2>Neighbourhoods</h2>
+    <ul>
+      <li>Neighbourhood1: 100</li>
+      <li>Neighbourhood2: 98</li>
+      <li>Neighbourhood3: 90</li>
+      <li>Neighbourhood4: 50</li>
+      <li>Neighbourhood5: 49</li>
+      <li>Neighbourhood6: 30</li>
+      <li>Neighbourhood7: 10</li>
+    </ul>
+    <p>Compare scores:</p>
+    <input
+      type="radio"
+      id="local"
+      name="areaComparison"
+      value="local"
+      checked
+    />
+    <label for="local">Locally</label>
+    <input type="radio" id="national" name="areaComparison" value="national" />
+    <label for="national">Nationally</label>
+  </div>
+);
 
 export default NeighbourhoodInfo;

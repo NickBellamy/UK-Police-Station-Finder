498f57d27486be4d4ca5999111fcf358bcde2c41
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-02 15:12:39 +0100

    feat: Add Google Maps map with placeholder data

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import apiConfig from './apiKeys';
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
 
-const Map = () => (
-  <div>
-      Map goes here...
-  </div>
-);
+class Map extends Component {
+  render() {
+    const CrimeMap = withScriptjs(
+      //Pass props in here..
+      withGoogleMap(() => (
+        <GoogleMap
+          defaultCenter={{ lat: 52.9548, lng: -1.1581 }}
+          defaultZoom={6}
+        >
+          <Marker position={{ lat: 52.9548, lng: -1.1581 }} />
+        </GoogleMap>
+      ))
+    );
+
+    return (
+      <CrimeMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
+          apiConfig.googleMapsKey
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `70vh`, width: '100%' }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
 
 export default Map;

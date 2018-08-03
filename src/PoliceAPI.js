94918a649e7fac973ea9f9bcb096bd873dc7043e
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-03 11:40:20 +0100

    feat: Pull neighbourhood locations from API

--- src/PoliceAPI.js
+++ src/PoliceAPI.js
@@ -1,20 +1,23 @@
 const api = 'https://data.police.uk/api';
 
-export const getForces = () => (
-    fetch(`${api}/forces`)
-    .then(res => res.json())
-)
+export const getForces = () => fetch(`${api}/forces`).then(res => res.json());
 
-export const getCrimes = () => (
-    fetch(`${api}/crime-categories`)
-    .then(res => res.json())
-)
+export const getCrimes = () =>
+  fetch(`${api}/crime-categories`).then(res => res.json());
 
 export const getNeighbourhoods = area => (
-    fetch(`${api}/${area}/neighbourhoods`)
+  fetch(`${api}/${area}/neighbourhoods`)
     .then(res => res.json())
-    .then(res => res.map(hood => 
-        ({name: hood.name.replace('&amp;', '&'),id: hood.id})
-      )
+    .then(res => res.map(hood => hood.id))
+    .then(hoodIds =>
+      Promise.all(hoodIds.map(id => fetch(`${api}/${area}/${id}`)))
     )
-)
+    .then(res => Promise.all(res.map(hood => hood.json())))
+    .then(hoods =>
+      hoods.map(hood => ({
+        id: hood.id,
+        name: hood.name,
+        location: hood.centre
+      }))
+    )
+);

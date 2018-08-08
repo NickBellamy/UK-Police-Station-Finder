91c565c30830d53751471036f4ed65a5da907439
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-08 15:13:37 +0100

    fix: Fix syntax error

--- src/Map.js
+++ src/Map.js
@@ -21,7 +21,7 @@ const RenderMap = withScriptjs(
         >
           {props.selectedNeighbourhood === hood.id ? (
             <InfoWindow onCloseClick={() => props.selectNeighbourhood('')}>
-              <div class="contact_details" style={{ maxWidth: `300px` }}>
+              <div className="contact_details" style={{ maxWidth: `300px` }}>
                 <h3>{hood.name}</h3>
                 <ul>
                   <li>

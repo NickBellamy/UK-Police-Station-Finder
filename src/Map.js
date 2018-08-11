5cc5ccee4754b3cb904a1b4f78244b0d91e3ba5b
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-11 15:33:42 +0100

    feat: Zoom into selected area

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { renderContact } from './helpers'
+import { renderContact } from './helpers';
 import apiConfig from './apiKeys';
 import {
   withGoogleMap,
@@ -11,46 +11,61 @@ import {
 } from 'react-google-maps';
 
 const RenderMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap defaultCenter={{ lat: 52.9548, lng: -1.1581 }} defaultZoom={6}>
-      {props.neighbourhoods.map(hood => (
-        <Marker
-          key={hood.id}
-          animation={hood.id === props.selectedNeighbourhood ? 1 : null}
-          position={hood.location}
-          onClick={() => props.selectNeighbourhood(hood.id)}
-        >
-          {props.selectedNeighbourhood === hood.id ? (
-            <InfoWindow onCloseClick={() => props.selectNeighbourhood('')}>
-              <div className="contact_details" style={{ maxWidth: `300px` }}>
-                <h3>{hood.name}</h3>
-                <ul>
-                  {(hood.website && !hood.contact.website) && (
-                    <li>
-                      <span>website:</span>{' '}
-                      <a href={hood.website} target="_blank">
-                        {hood.website}
-                      </a>
-                    </li>
-                  )}
-                  {Object.keys(hood.contact)
-                    .map(key => ({ type: key, details: hood.contact[key] }))
-                    .map(contact => renderContact(contact))
-                    .map(contactType => (
-                      <li key={contactType.type}>
-                        <span>{contactType.type}:</span> {contactType.details}
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </InfoWindow>
-          ) : (
-            ''
-          )}
-        </Marker>
-      ))}
-    </GoogleMap>
-  ))
+  withGoogleMap(props => {
+    let bounds = new window.google.maps.LatLngBounds();
+    props.neighbourhoods.map(hood => {
+      bounds.extend(new window.google.maps.LatLng(
+        hood.location.lat,
+        hood.location.lng
+      ));
+    });
+
+    return (
+      <GoogleMap
+        defaultCenter={{ lat: 52.9548, lng: -1.1581 }}
+        defaultZoom={6}
+        ref={map => map && map.fitBounds(bounds)}
+      >
+        {props.neighbourhoods.map(hood => (
+          <Marker
+            key={hood.id}
+            animation={hood.id === props.selectedNeighbourhood ? 1 : null}
+            position={hood.location}
+            onClick={() => props.selectNeighbourhood(hood.id)}
+          >
+            {props.selectedNeighbourhood === hood.id ? (
+              <InfoWindow onCloseClick={() => props.selectNeighbourhood('')}>
+                <div className="contact_details" style={{ maxWidth: `300px` }}>
+                  <h3>{hood.name}</h3>
+                  <ul>
+                    {hood.website &&
+                      !hood.contact.website && (
+                        <li>
+                          <span>website:</span>{' '}
+                          <a href={hood.website} target="_blank">
+                            {hood.website}
+                          </a>
+                        </li>
+                      )}
+                    {Object.keys(hood.contact)
+                      .map(key => ({ type: key, details: hood.contact[key] }))
+                      .map(contact => renderContact(contact))
+                      .map(contactType => (
+                        <li key={contactType.type}>
+                          <span>{contactType.type}:</span> {contactType.details}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </InfoWindow>
+            ) : (
+              ''
+            )}
+          </Marker>
+        ))}
+      </GoogleMap>
+    );
+  })
 );
 
 class Map extends Component {

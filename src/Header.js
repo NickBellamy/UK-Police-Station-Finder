1053d4ca9cfe5a06a121daced32d89c34115d114
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-14 13:09:49 +0100

    refactor: App.css

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="App-header">
-    <h1 className="App-title">UK Police Station Finder</h1>
+  <header id="app_header">
+    <h1>UK Police Station Finder</h1>
   </header>
 );
 

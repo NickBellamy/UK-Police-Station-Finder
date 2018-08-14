ae32046234cb211ffa944251b5c1f1eb759f0b02
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-08-14 17:37:43 +0100

    feat: Add service worker back in

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './css/index.css';
 import App from './App';
-//Service worker not currently used
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//Service worker not currently used
-//registerServiceWorker();
+registerServiceWorker();
